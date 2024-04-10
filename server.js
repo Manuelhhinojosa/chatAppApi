2e07137a29c89bb61a0d454d7ab706cc6d9046ef
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-04-10 15:58:57 +0700

    npm installed dotend package, added .env file to root dir, and in it added PORT var. Used PORT var inside server.js. Tested server via browser and postman app. all okay

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send("chapAppApi API ");
+  res.send("chapAppApi API running successfully.");
 });
 
-app.listen(3000, console.log("Server started on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
