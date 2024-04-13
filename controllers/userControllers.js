5c7c0dacb8ba1fd448d9ca9e5c82b9f2e8c5f09d
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-04-13 12:54:16 +0700

    {

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,43 @@
 // model
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 
 // functions
-const signupUser = (req, res) => {};
+const signupUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all the fields");
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    res.status(400);
+    throw new Error("User alredy exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user");
+  }
+});
+
 const loginUser = (req, res) => {};
 
 // exporting functions
